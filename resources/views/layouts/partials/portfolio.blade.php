c13c68726e57378824ff1c4c86c4812b89146c2d
Dung.Kaii <dungntph33609@fpt.edu.vn>
2024-07-20 00:36:27 +0700

    update

--- resources/views/layouts/partials/portfolio.blade.php
+++ resources/views/layouts/partials/portfolio.blade.php
@@ -56,7 +56,7 @@
                                 <a href="{{ $iphone->image }}" title="App 1" data-gallery="portfolio-gallery-app" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                                 <a href="portfolio-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a>
                             </div>
-                     
+
                     </div><!-- End Portfolio Item -->
                     @endforeach
 
